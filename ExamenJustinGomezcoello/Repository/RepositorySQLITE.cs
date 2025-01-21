13611d599c01d56998d14df2c67f51d8892dc56f
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2025-01-21 07:38:45 -0500

    Repositorio para guardar los datos del modelo pelicula en sqlite

---
+++ ExamenJustinGomezcoello/Repository/RepositorySQLITE.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ExamenJustinGomezcoello.Models;
+
+namespace ExamenJustinGomezcoello.Repository
+{
+    public class RepositorySQLITE
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public RepositorySQLITE(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Pelicula>().Wait();
+        }
+
+        public Task<int> InsertarPaisAsync(Pelicula pelicula)
+        {
+            return _database.InsertAsync(pelicula);
+        }
+
+        public Task<List<Pelicula>> ObtenerPaisesAsync()
+        {
+            return _database.Table<Pelicula>().ToListAsync();
+        }
+
+
+    }
+}
