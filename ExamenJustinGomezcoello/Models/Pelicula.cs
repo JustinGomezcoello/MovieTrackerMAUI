736c8c0e1c05554d971655d7790103e49fd4cf96
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2025-01-21 07:36:26 -0500

    Contenido para mi modelo Pelicual donde hace referencia a todos los datos que quiero obtener de la api

---
+++ ExamenJustinGomezcoello/Models/Pelicula.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace ExamenJustinGomezcoello.Models
+{
+    public class Pelicula
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+        public string Genero { get; set; } = string.Empty;
+        public string Actor { get; set; } = string.Empty;
+        public string Premios { get; set; } = string.Empty;
+        public string Sitio { get; set; } = string.Empty;
+        public string Usuario { get; set; } = "JGomezcoello";
+
+        [Ignore]
+        public string Detalles { get; set; } = string.Empty;
+    }
+}
